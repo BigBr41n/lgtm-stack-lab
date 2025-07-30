fca15e351ad91ee50569b60b2eab92dc43601be4
BigBr41 <mer.r4him@gmail.com>
2025-07-30 21:48:15 +0100

    feat: add models (order & ApiResponse)

---
+++ internal/models/models.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type Order struct {
+	ID         string    `json:"id"`
+	CustomerID string    `json:"customer_id"`
+	Amount     float64   `json:"amount"`
+	Status     string    `json:"status"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+type APIResponse struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+	TraceID string      `json:"trace_id"`
+}
